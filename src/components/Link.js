0d2e86f70553fdf2f08d552e5ade0b3a184d4e51
Bryan Geil <beg692@gmail.com>
2020-01-23 18:39:23 -0600

    finished presentational components

---
+++ src/components/Link.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Link = ({ active, chikdren, onCLick }) => {
+	if(active) {
+		return <span>{children}</span>
+	}
+	return (
+		<a 
+		 href=""
+		 onClick={e => {
+		 	e.preventDefault()
+		 	onClick()
+		 }}
+		 >
+		 {children}
+		 </a>
+	)
+}
+
+Link.propTypes = {
+	active: PropTypes.bool.isRequired,
+	chidlren: PropTypes.node.isRequired,
+	onClick: PropTypes.func.isRequired
+}
+
+export default Link
