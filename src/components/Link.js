111ee5b95fb31d1daa5370e7500f6f4b457134d9
Bryan Geil <beg692@gmail.com>
2020-01-28 14:47:10 -0600

    fixed errors in code

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Link = ({ active, chikdren, onCLick }) => {
-	if(active) {
-		return <span>{children}</span>
-	}
-	return (
-		<a 
-		 href=""
-		 onClick={e => {
-		 	e.preventDefault()
-		 	onClick()
-		 }}
-		 >
-		 {children}
-		 </a>
-	)
-}
-
+const Link = ({ active, children, onClick }) => (
+  <button
+    onClick={onClick}
+    disabled={active}
+    style={{
+      marginLeft: '4px'
+    }}
+  >
+    {children}
+  </button>
+)
 Link.propTypes = {
-	active: PropTypes.bool.isRequired,
-	chidlren: PropTypes.node.isRequired,
-	onClick: PropTypes.func.isRequired
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
 }
-
 export default Link
--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,35 +1,26 @@
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
-
+import { VisibilityFilters } from '../actions'
 const getVisibleTodos = (todos, filter) => {
-	switch (filter) {
-		case 'SHOW ALL':
-			return todos
-		case 'SHOW_COMPLETED':
-			return todos.filter(t => t.completed)
-		case 'SHOW_ACTIVE':
-			return todos.filter( t => !t.completed)
-	}
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(t => t.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed)
+    default:
+      throw new Error('Unknown filter: ' + filter)
+  }
 }
-
-const mapStatetoProps = state => {
-	return {
-		todos: getVisibleTodos(state.todos, state.visibilityFilter)
-	}
-}
-
-const mapDispatchToProps = dispatch => {
-	return {
-		onTodoClick: id => {
-			dispatch(toogleTodo(id))
-		}
-	}
-}
-
-const VisibleTodoList = connect(
-	mapStatetoProps,
-	mapDispatchToProps
-	)(TodoList)
-
-export default VisibleTodoList
+const mapStateToProps = state => ({
+  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+})
+const mapDispatchToProps = dispatch => ({
+  toggleTodo: id => dispatch(toggleTodo(id))
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
